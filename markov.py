5161a3f07839c13207b4b63feccd4148f22e40f4
pascalevain <138302687+pascalevain@users.noreply.github.com>
2025-03-27 13:00:53 +0100

    Update markov.py

--- markov.py
+++ markov.py
@@ -1,11 +1,11 @@
-
 import numpy as np
 
 def analyse_markov(historique):
     resultats = {}
-    for i in range(len(historique)):
-        nums = list(range(1, 6))
-        stars = list(range(1, 3))
-        score = np.random.uniform(0, 1)
-        resultats[i] = {"nums": nums, "stars": stars, "score": score}
+    for i in range(10):
+        resultats[i] = {
+            "nums": list(np.random.choice(range(1, 51), 5, replace=False)),
+            "stars": list(np.random.choice(range(1, 13), 2, replace=False)),
+            "score": np.random.rand()
+        }
     return resultats
