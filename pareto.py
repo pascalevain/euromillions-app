24bc5c88e49feb6e5e1ebeb4199c890e2297f183
pascalevain <138302687+pascalevain@users.noreply.github.com>
2025-03-27 13:01:50 +0100

    Update pareto.py

--- pareto.py
+++ pareto.py
@@ -1,11 +1,20 @@
-
 def score_pareto(markov, arima, context, meta, n_large, n_croisee, n_recent):
     grilles = []
-    for i in range(len(markov)):
-        if i in arima and i in context and i in meta:
-            score = markov[i]['score'] + arima[i]['score'] + context[i]['score'] + meta[i]['score']
-            nums = markov[i]['nums']
-            stars = markov[i]['stars']
+
+    for i in markov:
+        try:
+            score = (
+                markov[i]["score"]
+                + arima[i]["score"]
+                + context[i]["score"]
+                + meta[i]["score"]
+            )
+            nums = markov[i]["nums"]
+            stars = markov[i]["stars"]
             grilles.append((nums, stars, score))
-    grilles.sort(key=lambda x: x[2], reverse=True)
+        except Exception as e:
+            print(f"Erreur à l'index {i}: {e}")
+            continue
+
+    grilles = sorted(grilles, key=lambda x: x[2], reverse=True)
     return grilles[:n_large + n_croisee + n_recent]
